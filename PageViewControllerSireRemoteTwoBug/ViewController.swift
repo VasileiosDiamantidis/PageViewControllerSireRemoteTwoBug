35b6adc52d31bff127cc476c28aa756e9ea7dbcf
VasileiosDiamantidis <vasileios.diamantidis@zattoo.com>
2021-07-28 10:49:58 +0200

    Initial Commit

---
+++ PageViewControllerSireRemoteTwoBug/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PageViewControllerSireRemoteTwoBug
+//
+//  Created by Vasileios on 28.07.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
